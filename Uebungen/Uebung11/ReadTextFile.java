f2488859cf7e1b7b9539f535ed50e35cd58006ae
m.kubbillum <m.kubbillum@stud.hs-wismar.de>
2022-01-10 18:14:51 +0100

    Einarbeitung

--- Uebungen/Uebung11/ReadTextFile.java
+++ Uebungen/Uebung11/ReadTextFile.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,8 +10,7 @@ public class ReadTextFile {
 	public static void main(String[] args) {
 		BufferedReader in = null;
 		try {
-			URL url = ReadTextFile.class.getResource("textdatei.txt");
-			in = new BufferedReader(new FileReader(url.getPath()));
+			in = new BufferedReader(new FileReader("textdatei.txt"));
 			String s = "";
 			String res;
 			while ((res = in.readLine()) != null) {
