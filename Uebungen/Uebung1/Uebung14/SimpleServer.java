91673f40c371ceb0ec4da9ed00a50c1048348532
m.kubbillum <m.kubbillum@stud.hs-wismar.de>
2022-01-14 12:09:59 +0100

    Übung 2	Aufgabe 3: Paralleler Server mit Socket-Programmierung und Java Threads

--- Uebungen/Uebung1/Uebung14/SimpleServer.java
+++ Uebungen/Uebung1/Uebung14/SimpleServer.java
@@ -8,49 +8,27 @@ import java.net.Socket;
 
 public class SimpleServer {
 	public static void main(String[] args) {
-		try {
-			// Das try-with-resources Statement ermoeglicht die Deklaration von Ressourcen,
-			// die am Ende des try Blocks automatisch geschlossen werden.
-
-			// ServerSocket repraesentiert einen Server, dessen Konstruktor die Nummer des
-			// Ports uebergeben bekommt, an dem der Server horchen soll.
-			try (ServerSocket ss = new ServerSocket(8442)) { // Hochschulkennung st191442 => Port: 8442
-				System.out.println("START SimpleServer!");
-
-				String t = null;
-
-				// Die Variable stop dient als Bedindung, bei welcher der Server gestoppt bzw.
-				// die while-Schleife nicht fortgesetzt wird. Sie wird innerhalb des Servers auf
-				// true gesetzt, wenn dieser die Nachricht "stop" vom Client empfaengt.
-				boolean stop = false;
-
-				while (!stop) {
-
-					// Nach Einrichten des Sockets wird durch die Methode accept() eine Verbindung
-					// innerhalb einer Endlosschleife hergestellt.
-					Socket s = ss.accept();
-
-					// Innerhalb der while-Schleife werden auf gleiche Weise, wie in
-					// SimpleClient.java beschrieben, Input- und OutputStreams gelesen und
-					// geschrieben. Diese werden wieder vom Socket geliefert.
-					BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-					PrintStream out = new PrintStream(s.getOutputStream());
-					t = in.readLine();
-					if (t != null) {
-						System.out.println("Server receives: " + t);
-						if (!"stop".equals(t)) {
-							out.println("Hi client!");
-						} else {
-							stop = true;
-							out.println("The server will be stopped!");
-						}
-						out.flush();
+		try (ServerSocket ss = new ServerSocket(8442)) { // Hochschulkennung st191442 => Port: 8442
+			System.out.println("START SimpleServer!");
+			String t = null;
+			boolean stop = false;
+			while (!stop) {
+				Socket s = ss.accept();
+				BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+				PrintStream out = new PrintStream(s.getOutputStream());
+				t = in.readLine();
+				if (t != null) {
+					System.out.println("Server receives: " + t);
+					if (!"stop".equals(t)) {
+						out.println("Hi client!");
+					} else {
+						stop = true;
+						out.println("The server will be stopped!");
 					}
+					out.flush();
 				}
-				System.out.println("STOP SimpleServer!");
 			}
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
+			System.out.println("STOP SimpleServer!");
 		}
 	}
 }
