7603f0e68a98670c82c8fdaa3179cfeecfbefb32
m.kubbillum <m.kubbillum@stud.hs-wismar.de>
2022-02-25 14:02:28 +0100

    Zwischenstand Uebung 35f

---
+++ Uebungen/Uebung3/Uebung34/Uebung35f/Configuration.sql
@@ -0,0 +1,47 @@
+--sqlplus /nolog
+--1) 
+CONNECT aqadm/aqadm;
+
+--2) 
+EXEC DBMS_AQADM.CREATE_QUEUE_TABLE(queue_table => 'multi_message_table',queue_payload_type => 'SYS.AQ$_JMS_OBJECT_MESSAGE',multiple_consumers => TRUE);
+EXEC DBMS_AQADM.CREATE_QUEUE(queue_name => 'multi_queue',queue_table => 'multi_message_table');
+EXEC DBMS_AQADM.START_QUEUE(queue_name => 'multi_queue');
+
+--3a) CREATE ROLE my_aq_adm_role;
+--3b) GRANT CONNECT, RESOURCE, aq_administrator_role TO my_aq_adm_role;
+
+--4a) 
+CREATE ROLE my_aq_user_role;
+--4b) 
+GRANT CREATE SESSION, aq_user_role TO my_aq_user_role;
+--4c)
+EXEC DBMS_AQADM.GRANT_SYSTEM_PRIVILEGE(privilege => 'ENQUEUE_ANY',grantee => 'my_aq_user_role',admin_option => FALSE);
+--4d) 
+EXEC DBMS_AQADM.GRANT_SYSTEM_PRIVILEGE(privilege => 'DEQUEUE_ANY',grantee => 'my_aq_user_role',admin_option => FALSE);
+
+--5a.1) Falls bei 5a.2: "ORA-00959: tablespace 'TAB' does not exist"
+--CREATE TABLESPACE tab DATAFILE 'tab_data.dbf' SIZE 10m;
+--5a.2) 
+CREATE USER aqadm IDENTIFIED BY aqadm DEFAULT TABLESPACE tab TEMPORARY TABLESPACE temp;
+--5b) 
+GRANT my_aq_adm_role TO aqadm;
+
+--6a) 
+CREATE USER aquser IDENTIFIED BY aquser DEFAULT TABLESPACE tab TEMPORARY TABLESPACE temp;
+--6b) 
+GRANT my_aq_user_role TO aquser;
+
+--7) 
+CONNECT aqadm/aqadm;
+
+--8)
+CREATE TYPE queue_message_type AS OBJECT(no NUMBER,title VARCHAR2(30),text VARCHAR2(2000) );
+/
+GRANT EXECUTE ON queue_message_type TO my_aq_user_role;
+
+--9a) 
+EXEC DBMS_AQADM.CREATE_QUEUE_TABLE(queue_table => 'queue_message_table',queue_payload_type => 'aqadm.queue_message_type');
+--9b) 
+EXEC DBMS_AQADM.CREATE_QUEUE(queue_name => 'message_queue',queue_table => 'queue_message_table');
+--9c) 
+EXEC DBMS_AQADM.START_QUEUE(queue_name => 'message_queue');
