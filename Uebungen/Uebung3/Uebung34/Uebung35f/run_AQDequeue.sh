044834b914232f2b4824c0b1912178caa0c49afc
m.kubbillum <m.kubbillum@stud.hs-wismar.de>
2022-02-26 14:18:16 +0100

    AQJmsDequeue

--- Uebungen/Uebung3/Uebung34/Uebung35f/run_AQDequeue.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export CLASSPATH=".:/oracle/oracle112/JRE:/oracle/oracle112/jlib:/oracle/oracle112/rdbms/jlib:/oracle/oracle112/jdbc/lib/ojdbc5.jar:/oracle/oracle112/oc4j/sqlj/lib/translator.jar:/oracle/oracle112/oc4j/sqlj/lib/runtime12ee.jar:/oracle/oracle112/oc4j/rdbms/jlib/aqapi.jar:/oracle/oracle112/oui/jlib/classes12.jar:/oracle/oracle112/rdbms/jlib/jmscommon.jar:/oracle/oracle112/jlib/jta.jar"
-/oracle/oracle112/jdk/bin/java AQJmsPublisher
